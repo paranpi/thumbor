6ea3cb816696486db1b27dba271bbb4da0b5e60d
chris <chris@wishbeen.com>
2016-07-15 18:20:45 +0900

    wishbeen url signer

---
+++ thumbor/url_signers/wishbeen_url_signer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import base64
+import hashlib
+import hmac
+import re
+from thumbor.url_signers import BaseUrlSigner
+
+
+class UrlSigner(BaseUrlSigner):
+    """Validate urls and sign them using base64 hmac-sha1
+    """
+
+    def signature(self, url):
+        replaced_url = re.sub("\/smart\/.+\(\)\/", "/", url)
+
+        return base64.urlsafe_b64encode(
+            hmac.new(
+                self.security_key, unicode(replaced_url).encode('utf-8'), hashlib.sha1
+            ).digest()
+        )
